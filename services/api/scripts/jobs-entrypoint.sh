37b9c5993d2bac668af466abe2f4730f881b4c82
Dominiek Ter Heide <info@dominiek.com>
2019-05-20 18:32:20 -0400

    Fix for api jobs entrypoint

--- services/api/scripts/jobs-entrypoint.sh
+++ services/api/scripts/jobs-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Setting crontab"
 env >> /etc/environment
-# Every 15 minutes
-echo "*/15 * * * * cd /api; node scripts/jobs.js >> /api/crontab.log 2>> /api/crontab.log" >> cron
+# Every 5 minutes
+echo "*/5 * * * * cd /api; node scripts/jobs.js >> /api/crontab.log 2>> /api/crontab.log" >> cron
 crontab cron
 rm cron
 echo "Running all jobs for first time"
-node /api/scripts/jobs.js >> /api/crontab.log 2>> /api/crontab.log
+node /service/scripts/jobs.js >> /service/crontab.log 2>> /service/crontab.log
 crond &&
-tail -f /api/crontab.log
+tail -f /service/crontab.log
