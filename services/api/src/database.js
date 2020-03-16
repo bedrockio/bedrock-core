8c879277f35df2f7f633a80f8d47980f4fd605aa
Dominiek Ter Heide <info@dominiek.com>
2020-03-16 12:30:46 -0400

    Fix for topology destoryed mongo issues in kubernetes deployments

--- services/api/src/database.js
+++ services/api/src/database.js
@@ -7,7 +7,11 @@ module.exports = async () => {
   await mongoose.connect(config.get('MONGO_URI'), {
     useFindAndModify: false,
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    // sets how many times to try reconnecting
+    reconnectTries: Number.MAX_VALUE,
+    // sets the delay between every retry (milliseconds)
+    reconnectInterval: 2000
   });
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
