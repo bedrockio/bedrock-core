5b26fd75ae938f4e29eb5eb7a964770056612d31
Andrew Plummer <plummer.andrew@gmail.com>
2020-05-12 19:04:46 +0900

    thumnail -> thumbnail

--- services/web/src/utils/uploads.js
+++ services/web/src/utils/uploads.js
@@ -2,6 +2,6 @@ import { API_URL } from 'utils/env';
 
 export function urlForUpload(upload, thumbnail = false) {
   let url = `${API_URL}/1/uploads/${upload.hash}/image`;
-  if (thumbnail) url += '?thumnail=true';
+  if (thumbnail) url += '?thumbnail=true';
   return url;
 }
