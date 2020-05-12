a89768c23e8156b43a5b419aaff30e7dda60f9d2
Andrew Plummer <plummer.andrew@gmail.com>
2020-05-12 19:13:19 +0900

    updated urlForUpload to use URL

--- services/web/src/utils/uploads.js
+++ services/web/src/utils/uploads.js
@@ -1,7 +1,11 @@
 import { API_URL } from 'utils/env';
 
 export function urlForUpload(upload, thumbnail = false) {
-  let url = `${API_URL}/1/uploads/${upload.hash}/image`;
-  if (thumbnail) url += '?thumbnail=true';
-  return url;
+  const url = new URL(`/1/uploads/${upload.hash}/image`, API_URL);
+  if (thumbnail) {
+    url.search = new URLSearchParams({
+      thumbnail: true
+    });
+  }
+  return url.toString();
 }
