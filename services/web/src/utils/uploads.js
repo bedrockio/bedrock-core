7d5051e32fea76f8f716c0272c2c96ce728b36a9
Dominiek Ter Heide <info@dominiek.com>
2020-05-11 21:09:53 -0400

    Fix for trailing space issue on uploads

--- services/web/src/utils/uploads.js
+++ services/web/src/utils/uploads.js
@@ -1,7 +1,7 @@
 import config from 'config';
 
 export function urlForUpload(upload, thumbnail = false) {
-  let url = `${config.API_URL}1/uploads/${upload.hash}/image`;
+  let url = `${config.API_URL}/1/uploads/${upload.hash}/image`;
   if (thumbnail) url += '?thumnail=true';
   return url;
 }
