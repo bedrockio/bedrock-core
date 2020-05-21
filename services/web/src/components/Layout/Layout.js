e24c1c901458e99fbe4e3f0ac9cdee6d80c12b08
Andrew Plummer <plummer.andrew@gmail.com>
2020-05-21 22:40:38 +0900

    added documentation for Layout

--- services/web/src/components/Layout/Layout.js
+++ services/web/src/components/Layout/Layout.js
@@ -1,3 +1,75 @@
+// Component that wraps simple flexbox layouts.
+//
+// Usage:
+//
+// 1. Simple horizontal layout that aligns its children vertically center:
+//
+// <Layout horizontal center>
+//   <el />
+//   <el />
+// </Layout>
+//
+// display: flex;
+// flex-flow: row;
+// align-items: center;
+//
+//
+// 2. A horizontal spread layout that pushes children to their edges:
+//
+// <Layout horizontal center spread>
+//   <el />
+//   <el />
+// </Layout>
+//
+// display: flex;
+// flex-flow: row;
+// align-items: center;
+// justify-content: space-between;
+//
+//
+// 3. A perfectly centered layout that aligns both vertically and horizontally center:
+//
+// <Layout center>
+//   <el />
+// </Layout>
+//
+// display: flex;
+// align-items: center;
+// justify-content: center;
+//
+//
+// 4. Other layout patterns can be achieved by mixing vertical and horizontal props:
+//
+// <Layout horizontal top />
+// <Layout horizontal center />
+// <Layout horizontal bottom />
+// <Layout vertical left />
+// <Layout vertical center />
+// <Layout vertical right />
+//
+//
+// 5. Grouping content:
+//
+// <Layout center>
+//   <Layout.Group>
+//     <el />
+//     <el />
+//   </Layout.Group>
+//   <Layout.Group>
+//     <el />
+//     <el />
+//   </Layout.Group>
+// </Layout>
+//
+// 6. Other props:
+//
+// stackable - Like semantic UI stacks on mobile
+// wrap - Allows wrapping of content
+// stretch - Applied align-items: stretch (children grow vertically or horizontally)
+// padded - Applies padding between items. (also <Layout extra padded />).
+// baseline - Applies align-items: baseline.
+// reversed - Layouts can be reversed.
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import { omit } from 'lodash';
