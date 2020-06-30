bb780075ee931a79f1d33849bfab2bbe186fecf6
Dominiek Ter Heide <info@dominiek.com>
2020-06-30 17:52:19 -0400

    Support for private array definitions in Mongoose

--- services/api/src/utils/Schema.js
+++ services/api/src/utils/Schema.js
@@ -1,35 +1,40 @@
 const mongoose = require('mongoose');
 
 class Schema extends mongoose.Schema {
-
   constructor(schema, options = {}) {
-    super({
-      deletedAt: { type: Date },
-      ...schema,
-    }, {
-
-      // Include timestamps by default.
-      timestamps: true,
-
-      // Export "id" getter and omit "__v" as
-      // well as private fields.
-      toJSON: {
-        getters: true,
-        versionKey: false,
-        transform: (doc, ret) => {
-          for (let key of Object.keys(ret)) {
-            const field = doc.schema.obj[key];
-            // Omit any key with a private prefix "_" or marked
-            // "access": "private" in the schema. Note that virtuals are
-            // excluded by default so they don't need to be removed.
-            if (key[0] === '_' || (field && field.access === 'private')) {
-              delete ret[key];
-            }
-          }
-        }
+    super(
+      {
+        deletedAt: { type: Date },
+        ...schema,
       },
-      ...options
-    });
+      {
+        // Include timestamps by default.
+        timestamps: true,
+
+        // Export "id" getter and omit "__v" as
+        // well as private fields.
+        toJSON: {
+          getters: true,
+          versionKey: false,
+          transform: (doc, ret) => {
+            for (let key of Object.keys(ret)) {
+              const field = doc.schema.obj[key];
+              // Support for arrays
+              if (field && field.length && field[0] && field[0].access === 'private') {
+                ret[key] = [];
+              }
+              // Omit any key with a private prefix "_" or marked
+              // "access": "private" in the schema. Note that virtuals are
+              // excluded by default so they don't need to be removed.
+              if (key[0] === '_' || (field && field.access === 'private')) {
+                delete ret[key];
+              }
+            }
+          },
+        },
+        ...options,
+      }
+    );
     Object.assign(this.methods, {
       delete: this.delete,
     });
@@ -39,7 +44,6 @@ class Schema extends mongoose.Schema {
     this.deletedAt = new Date();
     return this.save();
   }
-
 }
 
 module.exports = Schema;
