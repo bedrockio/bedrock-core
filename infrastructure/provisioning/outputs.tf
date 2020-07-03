83bec57a49c40bc8af383139655a44315346c9c8
J.C. Stevens <beatlevic@gmail.com>
2020-07-03 17:02:08 +0200

    Updated provisioning outputs with cli_connect command

--- infrastructure/provisioning/outputs.tf
+++ infrastructure/provisioning/outputs.tf
@@ -5,3 +5,7 @@ output "endpoint" {
 output "master_version" {
   value = google_container_cluster.default.master_version
 }
+
+output "cli_connect" {
+  value = "'gcloud container clusters get-credentials ${var.cluster_name} --zone ${var.region}-${var.zone} --project ${var.project}'"
+}
