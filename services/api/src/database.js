a0a274d4b8ec7495b33e19aeeb484315c6635f89
Kaare Larsen <me@kaareal.com>
2020-07-13 20:54:49 +0200

    updated copy

--- services/api/src/database.js
+++ services/api/src/database.js
@@ -5,15 +5,15 @@ mongoose.Promise = Promise;
 
 module.exports = async () => {
   await mongoose.connect(config.get('MONGO_URI'), {
-    // useNewUrlParser: The underlying MongoDB driver has deprecated their current connection string parser.
+    // The underlying MongoDB driver has deprecated their current connection string parser.
     useNewUrlParser: true,
-    // useCreateIndex: make Mongoose's default index build use createIndex() instead of ensureIndex()
+    // Make Mongoose's default index build use createIndex() instead of ensureIndex()
     // to avoid deprecation warnings from the MongoDB driver
     useCreateIndex: true,
-    // useUnifiedTopology: To opt in to using the MongoDB driver's new connection management engine.
+    // To opt in to using the MongoDB driver's new connection management engine.
     // https://mongoosejs.com/docs/deprecations.html#useunifiedtopology
     useUnifiedTopology: true,
-    // useFindAndModify: Set to false to make findOneAndUpdate() and findOneAndRemove()
+    // Set to false to make findOneAndUpdate() and findOneAndRemove()
     // use native findOneAndUpdate() rather than findAndModify()
     useFindAndModify: false,
   });
