c4458db05fe159d5bc5fd6fe02df5c7332c17a75
Dominiek Ter Heide <info@dominiek.com>
2020-07-28 19:52:05 -0400

    Triggering an asset bundle on web run, shaves off some loading seconds

--- services/web/serve.js
+++ services/web/serve.js
@@ -1,3 +1,4 @@
+const http = require('http');
 const Koa = require('koa');
 const webpack = require('koa-webpack');
 const webpackConfig = require('./webpack.config');
@@ -13,24 +14,33 @@ const { BIND_PORT, BIND_HOST, publicEnv } = require('./config');
   const webpackMiddleware = await webpack({
     hotClient: {
       host: BIND_HOST,
-      port: 34001
+      port: 34001,
     },
     devMiddleware: {
       watchOptions: {
-        ignored: /node_modules/
-      }
+        ignored: /node_modules/,
+      },
     },
     config: {
       ...webpackConfig,
-      mode: 'development'
-    }
+      mode: 'development',
+    },
   });
 
   app.use(terminateMiddleware('/assets/', webpackMiddleware));
   app.use(envMiddleware(publicEnv));
   app.use(historyMiddleware({ apps: ['/'] }));
   app.use(webpackMiddleware);
-  app.listen(BIND_PORT, BIND_HOST);
+  app.listen(BIND_PORT, BIND_HOST, () => {
+    console.info(`Running App on http://${BIND_HOST}:${BIND_PORT}/`);
 
-  console.info(`Running App on http://${BIND_HOST}:${BIND_PORT}/`);
+    // This is to trigger the build already (avoids 10 second loading on first hit)
+    console.info(`Bundling assets for the first time (hot loading enabled)`);
+    http.request({
+      method: 'GET',
+      host: BIND_HOST,
+      port: BIND_PORT,
+      path: '/',
+    });
+  });
 })();
