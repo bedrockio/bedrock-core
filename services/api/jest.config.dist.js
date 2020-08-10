8c39aea6f17140539111eddb065f62fc70c6bb99
J.C. Stevens <beatlevic@gmail.com>
2020-08-11 01:15:08 +0200

    Renamed ./built to ./dist. Added separate yarn test:dist to run jest only for ./dist

--- services/api/jest.config.dist.js
+++ services/api/jest.config.dist.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testTimeout: 60000,
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/src/'],
 };
