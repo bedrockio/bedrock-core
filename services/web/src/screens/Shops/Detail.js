6f19e0808381039542450890fb372df22f4c1433
Andrew Plummer <plummer.andrew@gmail.com>
2020-12-04 18:41:58 +0900

    separated screen routes using /:id/ to a separate Detail component

---
+++ services/web/src/screens/Shops/Detail.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import { Loader } from 'semantic-ui-react';
+import { Breadcrumbs, NotFound } from 'components';
+import { request } from 'utils/api';
+
+import List from './List';
+import Overview from './Overview';
+
+// --- Generator: imports
+import Products from './Products';
+// --- Generator: end
+
+export default class Shops extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      shop: null,
+      error: null,
+      loading: true,
+      onSave: this.onSave,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchShop();
+  }
+
+  componentDidUpdate(lastProps) {
+    const { id } = this.props.match.params;
+    if (id !== lastProps.match.params.id) {
+      this.fetchShop();
+    }
+  }
+
+  onSave = () => {
+    this.fetchShop();
+  }
+
+  async fetchShop() {
+    const { id } = this.props.match.params;
+    try {
+      this.setState({
+        error: null,
+        loading: true,
+      });
+      const { data } = await request({
+        method: 'GET',
+        path: `/1/shops/${id}`,
+      });
+      this.setState({
+        shop: data,
+        loading: false,
+      });
+    } catch (error) {
+      this.setState({
+        error,
+        loading: false,
+      });
+    }
+  }
+
+  render() {
+    const { loading, error } = this.state;
+    if (loading) {
+      return (
+        <Loader active>Loading</Loader>
+      );
+    } else if (error) {
+      return (
+        <React.Fragment>
+          <Breadcrumbs
+            link={<Link to="/shops">Shops</Link>}
+            active="Not Found"
+          />
+          <NotFound message="Sorry that shop wasn't found." />
+        </React.Fragment>
+      );
+    }
+    return (
+      <Switch>
+        <Route path="/shops" component={List} exact />
+        <Route
+          exact
+          path="/shops/:id"
+          render={(props) => (
+            <Overview {...props} {...this.state}  />
+          )}
+        />
+        {/* --- Generator: routes */}
+        <Route
+          exact
+          path="/shops/:id/products"
+          render={(props) => (
+            <Products {...props} {...this.state} />
+          )}
+        />
+        {/* --- Generator: end */}
+      </Switch>
+    );
+  }
+}
--- services/web/src/screens/Shops/index.js
+++ services/web/src/screens/Shops/index.js
@@ -1,105 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Switch, Route } from 'react-router-dom';
-import { Loader } from 'semantic-ui-react';
-import { Breadcrumbs, NotFound } from 'components';
-import { request } from 'utils/api';
 
 import List from './List';
-import Overview from './Overview';
-
-// --- Generator: imports
-import Products from './Products';
-// --- Generator: end
+import Detail from './Detail';
 
 export default class Shops extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      shop: null,
-      error: null,
-      onSave: this.fetchShop,
-    };
-  }
-
-  componentDidMount() {
-    this.fetchShop();
-  }
-
-  componentDidUpdate(lastProps) {
-    const { id } = this.props.match.params;
-    if (id !== lastProps.match.params.id) {
-      this.fetchShop();
-    }
-  }
-
-  fetchShop = async () => {
-    const { id } = this.props.match.params;
-    if (id) {
-      try {
-        this.setState({
-          shop: null,
-          error: null,
-        });
-        const { data } = await request({
-          method: 'GET',
-          path: `/1/shops/${id}`,
-        });
-        this.setState({
-          shop: data,
-        });
-      } catch (error) {
-        this.setState({
-          error,
-        });
-      }
-    } else {
-      this.setState({
-        shop: null,
-        error: null,
-      });
-    }
-  };
-
   render() {
-    const { id } = this.props.match.params;
-    const { shop, error } = this.state;
-    const loading = id && !shop && !error;
-    if (loading) {
-      return (
-        <Loader active>Loading</Loader>
-      );
-    } else if (error) {
-      return (
-        <React.Fragment>
-          <Breadcrumbs
-            link={<Link to="/shops">Shops</Link>}
-            active="Not Found"
-          />
-          <NotFound message="Sorry that shop wasn't found." />
-        </React.Fragment>
-      );
-    }
     return (
       <Switch>
         <Route path="/shops" component={List} exact />
-        <Route
-          exact
-          path="/shops/:id"
-          render={(props) => (
-            <Overview {...props} {...this.state}  />
-          )}
-        />
-        {/* --- Generator: routes */}
-        <Route
-          exact
-          path="/shops/:id/products"
-          render={(props) => (
-            <Products {...props} {...this.state} />
-          )}
-        />
-        {/* --- Generator: end */}
+        <Route path="/shops/:id" component={Detail} />
       </Switch>
     );
   }
