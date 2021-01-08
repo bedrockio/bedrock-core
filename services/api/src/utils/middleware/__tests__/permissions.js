c2d72ca3dbe59519614611e6bfd9c86362cb2930
Dominiek Ter Heide <info@dominiek.com>
2021-01-08 14:32:05 -0500

    Fix for testis

--- services/api/src/utils/middleware/__tests__/permissions.js
+++ services/api/src/utils/middleware/__tests__/permissions.js
@@ -1,11 +1,10 @@
 const { requirePermissions } = require('../permissions');
 const { setupDb, teardownDb, context } = require('../../testing');
-const { User, Role } = require('./../../../models');
+const { User } = require('./../../../models');
 
 beforeAll(async () => {
   await setupDb();
   await User.deleteMany({});
-  await Role.deleteMany({});
 });
 
 afterAll(async () => {
@@ -17,7 +16,7 @@ describe('permissions', () => {
     const authUser = await User.create({
       email: 'test@test.com',
     });
-    const middleware = requirePermissions({ context: 'global', endpoint: 'users', level: 'read' });
+    const middleware = requirePermissions({ scope: 'global', endpoint: 'users', permission: 'read' });
     let ctx;
     ctx = context({});
     await expect(middleware(ctx)).rejects.toHaveProperty('message', 'This endpoint requires authentication');
@@ -25,30 +24,23 @@ describe('permissions', () => {
     ctx.state = { authUser };
     await expect(middleware(ctx)).rejects.toHaveProperty(
       'message',
-      "You don't have the right permission for endpoint users (required level: global/read)"
+      "You don't have the right permission for endpoint users (required permission: global/read)"
     );
   });
   it('should allow global role based access control to certain endpoints', async () => {
-    const superAdminRole = await Role.create({
-      name: 'Super Admin',
-      context: 'global',
-      permissions: {
-        users: 'read-write',
-      },
-    });
     const superAdminUser = await User.create({
       email: 'admin@permissions.com',
       roles: [
         {
-          context: 'global',
-          role: superAdminRole._id,
+          scope: 'global',
+          role: 'superAdmin',
         },
       ],
     });
     const plainUser = await User.create({
       email: 'plain@permissions.com',
     });
-    const middleware = requirePermissions({ context: 'global', endpoint: 'users', level: 'read' });
+    const middleware = requirePermissions({ scope: 'global', endpoint: 'users', permission: 'read' });
     let ctx;
     ctx = context({});
     await expect(middleware(ctx)).rejects.toHaveProperty('message', 'This endpoint requires authentication');
@@ -56,7 +48,7 @@ describe('permissions', () => {
     ctx.state = { authUser: plainUser };
     await expect(middleware(ctx)).rejects.toHaveProperty(
       'message',
-      "You don't have the right permission for endpoint users (required level: global/read)"
+      "You don't have the right permission for endpoint users (required permission: global/read)"
     );
     ctx = context({});
     ctx.state = { authUser: superAdminUser };
