e2bfec891fb3bd588b98df7da65fc13ffe045217
Andrew Plummer <plummer.andrew@gmail.com>
2021-03-11 17:48:00 +0900

    added user tests

--- services/api/src/routes/__tests__/users.js
+++ services/api/src/routes/__tests__/users.js
@@ -128,9 +128,14 @@ describe('/1/users', () => {
       const user1 = await createUser({ name: 'One' });
       const user2 = await createUser({ name: 'Two' });
 
-      const response = await request('POST', '/1/users/search', {
-        ids: [user1.id, user2.id],
-      }, { user: admin });
+      const response = await request(
+        'POST',
+        '/1/users/search',
+        {
+          ids: [user1.id, user2.id],
+        },
+        { user: admin }
+      );
       expect(response.status).toBe(200);
       expect(response.body.data.length).toBe(2);
       expect(response.body.data[0].id).toBe(user2.id);
@@ -154,11 +159,50 @@ describe('/1/users', () => {
       const dbUser = await User.findById(user1.id);
       expect(dbUser.name).toEqual('new name');
     });
-    it('it should deny access to non-admins', async () => {
+    it('should deny access to non-admins', async () => {
       const user = await createUser({});
       const user1 = await createUser({ name: 'new name' });
       const response = await request('PATCH', `/1/users/${user1.id}`, { name: 'new name' }, { user });
       expect(response.status).toBe(401);
+    });
+    it('be able to update user roles', async () => {
+      const admin = await createUserWithRole('global', 'superAdmin');
+      const user1 = await createUser({ name: 'new name' });
+      const response = await request(
+        'PATCH',
+        `/1/users/${user1.id}`,
+        {
+          roles: [
+            {
+              role: 'limitedAdmin',
+              scope: 'global',
+            },
+          ],
+        },
+        { user: admin }
+      );
+      expect(response.status).toBe(200);
+      expect(response.body.data.roles.length).toBe(1);
+      expect(response.body.data.roles[0].role).toBe('limitedAdmin');
+      expect(response.body.data.roles[0].scope).toBe('global');
+      const dbUser = await User.findById(user1.id);
+      expect(dbUser.roles.length).toBe(1);
+      expect(dbUser.roles[0].role).toBe('limitedAdmin');
+      expect(dbUser.roles[0].scope).toBe('global');
+    });
+    it('should strip out reserved fields', async () => {
+      const admin = await createUserWithRole('global', 'superAdmin');
+      const user1 = await createUser({ name: 'new name' });
+      const response = await request(
+        'PATCH',
+        `/1/users/${user1.id}`,
+        { name: 'new name', id: 'fake id', createdAt: '2020-01-01T00:00:00Z', updatedAt: '2020-01-01T00:00:00Z' },
+        { user: admin }
+      );
+      expect(response.status).toBe(200);
+      expect(response.body.data.name).toBe('new name');
+      const dbUser = await User.findById(user1.id);
+      expect(dbUser.name).toEqual('new name');
     });
   });
 
