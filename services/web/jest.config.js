a9d509d2ffd79de338db173e6cd7e67f80a9e827
Andrew Plummer <plummer.andrew@gmail.com>
2021-04-07 20:07:41 +0900

    fix broken web tests

--- services/web/jest.config.js
+++ services/web/jest.config.js
@@ -4,8 +4,8 @@ module.exports = {
   moduleDirectories: ['node_modules', './src/', './serve'],
   testPathIgnorePatterns: ['node_modules', '\\w+.ignore.js'],
   moduleNameMapper: {
-    '\\.(css|less)$': '<rootDir>/src/utils/test/mocks/styles.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/utils/test/mocks/files.js',
+    '<rootDir>/__mocks__/fileMock.js',
   },
 };
