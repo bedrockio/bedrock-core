b45b056e38a7a7acfc2186e4556bac4123f62355
Andrew Plummer <plummer.andrew@gmail.com>
2021-04-21 21:01:53 +0900

    fixed some lingering issues

--- services/web/src/layouts/Sidebar/Content.js
+++ services/web/src/layouts/Sidebar/Content.js
@@ -3,6 +3,11 @@ import bem from 'helpers/bem';
 
 @bem
 export default class SidebarLayoutContent extends React.Component {
+  getModifiers() {
+    const { fixed } = this.context;
+    return [fixed ? 'fixed' : null];
+  }
+
   render() {
     return (
       <div className={this.getBlockClass()} onClick={this.context.close}>
