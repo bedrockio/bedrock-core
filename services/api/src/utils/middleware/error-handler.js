b45539c9b2cd35797a90173f8d5ca5365b59ef1d
Kaare Larsen <me@kaareal.com>
2021-05-03 09:21:39 +0200

    handling 3 party errors

--- services/api/src/utils/middleware/error-handler.js
+++ services/api/src/utils/middleware/error-handler.js
@@ -6,20 +6,22 @@ async function errorHandler(ctx, next) {
   try {
     await next();
   } catch (err) {
-    let { status, message, details } = err;
+    let { status, message, details, expose } = err;
 
-    if (!status) {
-      status = 500;
+    // `expose` is set if the error is triggered via ctx.throw or ctx.assert
+    // if its not set => assume its 3 party error and should trigger a 500
+    if (!expose || !status) {
       if (ENV_NAME === 'production') {
         message = 'An unexpected error occurred. Please try again later.';
       }
     }
+
     if (err.isJoi) {
       message = details.map((d) => d.message).join('\n');
     }
 
     ctx.type = 'json';
-    ctx.status = status;
+    ctx.status = status || 500;
     ctx.body = {
       error: { message, status, details },
     };
