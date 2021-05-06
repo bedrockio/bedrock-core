b2a04106c764fb12610385ce81047c535ee67193
Kaare Larsen <me@kaareal.com>
2021-05-06 19:00:58 +0200

    tweaks

--- services/web/serve/middleware/env.js
+++ services/web/serve/middleware/env.js
@@ -1,14 +1,11 @@
 // Injects environment variables into templates.
 // Note that only public variables should be exposed!
 const config = require('@bedrockio/config');
-const { pick } = require('lodash');
+const { omitBy } = require('lodash');
 
-const allConfig = config.getAll();
-const PUBLIC = pick(
-  allConfig,
-  Object.keys(allConfig).filter(
-    (key) => !key.startsWith('SERVER') && !key.startsWith('HTTP')
-  )
+const PUBLIC = omitBy(
+  config.getAll(),
+  (_, key) => key.startsWith('SERVER') || key.startsWith('HTTP')
 );
 
 module.exports = function envMiddleware() {
