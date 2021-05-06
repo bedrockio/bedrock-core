5d775e8e9aba58cb5edaf65e4fb3bd12dcad20b9
Kaare Larsen <me@kaareal.com>
2021-05-06 09:41:36 +0200

    requiring on start instead of runtime

--- services/api/src/utils/schema.js
+++ services/api/src/utils/schema.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const fs = require('fs');
 const path = require('path');
+const pluginAutoPopulate = require('mongoose-autopopulate');
+
 const { startCase, omitBy, isPlainObject } = require('lodash');
 const { ObjectId } = mongoose.Schema.Types;
 const { getJoiSchema, getMongooseValidator } = require('./validation');
@@ -89,7 +91,7 @@ function createSchema(attributes = {}, options = {}) {
     return this.save();
   };
 
-  schema.plugin(require('mongoose-autopopulate'));
+  schema.plugin(pluginAutoPopulate);
   return schema;
 }
 
