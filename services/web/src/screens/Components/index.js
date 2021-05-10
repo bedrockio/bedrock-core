bd3186e3ab1d6fe881afd62ae3771d0f8707aff2
Peter Cuhalev <peter@yukaii.com>
2021-05-10 13:00:06 +0200

    Adding more UI components

--- services/web/src/screens/Components/index.js
+++ services/web/src/screens/Components/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Header, Button, Message, Segment, Dropdown, Table } from 'semantic-ui-react';
+import { Header, Button, Message, Segment, Dropdown, Table, Label, Icon, Container, Grid, Divider, Menu } from 'semantic';
 import { screen } from 'helpers';
 import Spacer from '../../components/Layout/Spacer';
+import { Layout, Breadcrumbs } from 'components';
 
 const options = [
   { key: 1, text: 'Choice 1', value: 1 },
@@ -68,9 +69,13 @@ export default class ComponentsScreen extends React.Component {
           <Spacer size="small" />
 
           <Header size="medium">Variations</Header>
+          <Layout horizontal >
           <Dropdown placeholder='Selection' selection options={options} />
+          <div style={{ display:'inline-block', width: '16px' }}></div>
           <Dropdown placeholder='Search Selection' selection search options={options} />
+          <div style={{ display:'inline-block', width: '16px' }}></div>
           <Dropdown placeholder='Multiple Selection' multiple selection options={options} />
+          </Layout>
         </Segment>
 
         <Segment>
@@ -112,8 +117,282 @@ export default class ComponentsScreen extends React.Component {
               </Table.Row>
             </Table.Footer>
           </Table>
+        </Segment>
+
+        <Segment>
+          <Header size="large">Icon</Header>
+          <Spacer size="small" />
+
+          <Header size="medium">Variations</Header>
+
+          <Icon name="user" />
+          <Icon name="user" disabled />
+          <Icon name="user" color="blue" />
+          <Icon name="user" circular />
+          <Icon name="user" bordered />  
+          <Icon name="user" circular inverted color="green"/>  
+          <Icon name="user" bordered inverted />  
+        </Segment>
+
+        <Segment>
+          <Header size="large">Label</Header>
+          <Spacer size="small" />
+
+          <Header size="medium">Variations</Header>
+          <Label>
+            Standard label
+          </Label>
+          <Label basic>
+            Basic label
+          </Label>
+          <Label circular>
+            Circular label
+          </Label>
+
+          <Header size="medium">Options</Header>
+        
+          <Label>
+            <Icon name="envelope" /> Label with icon
+          </Label>
+
+          <Label as='a'>
+            Label as Link <Icon name='times' />
+          </Label>
+
+          <Label color="blue">
+            Colored label
+          </Label>
+        </Segment>
+
+        <Segment>
+          <Header size="large">Segment</Header>
+          <Spacer size="small" />
+
+          <Header size="medium">Variations</Header>
+
+          <Segment>
+            Standard Segment
+          </Segment>
+
+          <Segment placeholder>
+            Placeholder Segment
+          </Segment>
+
+          <Segment raised>
+            Raised Segment
+          </Segment>
 
         </Segment>
+
+        <Segment>
+          <Header size="large">Container & Grid</Header>
+          <Spacer size="small" />
+
+          <Header size="medium">Container</Header>
+
+          <Container>
+            <div>Content within a Container</div>
+          </Container>
+          
+          <Spacer size="small" />
+          
+          <h4>16 Columns Grid</h4>
+            <Grid>
+              <Grid.Column><Label>1</Label></Grid.Column>
+              <Grid.Column><Label>2</Label></Grid.Column>
+              <Grid.Column><Label>3</Label></Grid.Column>
+              <Grid.Column><Label>4</Label></Grid.Column>
+              <Grid.Column><Label>5</Label></Grid.Column>
+              <Grid.Column><Label>6</Label></Grid.Column>
+              <Grid.Column><Label>7</Label></Grid.Column>
+              <Grid.Column><Label>8</Label></Grid.Column>
+              <Grid.Column><Label>9</Label></Grid.Column>
+              <Grid.Column><Label>10</Label></Grid.Column>
+              <Grid.Column><Label>11</Label></Grid.Column>
+              <Grid.Column><Label>12</Label></Grid.Column>
+              <Grid.Column><Label>13</Label></Grid.Column>
+              <Grid.Column><Label>14</Label></Grid.Column>
+              <Grid.Column><Label>15</Label></Grid.Column>
+              <Grid.Column><Label>16</Label></Grid.Column>
+            </Grid>
+            </Segment>
+
+          
+        <Segment>
+          <Header size="large">Header</Header>
+          <Spacer size="small" />
+
+          <Header size="medium">Page Variations</Header>
+
+          <Header as="h1">
+            h1 Header
+          </Header>
+
+          <Header as="h2">
+            h2 Header
+          </Header>
+
+          <Header as="h3">
+            h3 Header
+          </Header>
+
+          <Header as="h4">
+            h4 Header
+          </Header>
+
+          <Header as="h5">
+            h5 Header
+          </Header>
+
+          <Header as="h6">
+            h6 Header
+          </Header>
+
+          <div style={{ display:'block', height:'5px' }} />
+          <Divider />
+
+          <Header size="medium">Content Variations</Header>
+
+          <Header size="tiny">
+            Tiny Header
+          </Header>
+
+          <Header size="small">
+            Small Header
+          </Header>
+
+          <Header size="medium">
+            Medium Header
+          </Header>
+
+          <Header size="large">
+            Large Header
+          </Header>
+
+          <Header size="huge">
+            Huge Header
+          </Header>
+
+        </Segment>
+
+        <Segment>
+          <Header size="large">Menu</Header>
+          <Spacer size="small" />
+
+          <Header size="medium">Variations</Header>
+
+          <Header size="small">Primary</Header>
+
+          <Menu primary>
+            <Menu.Item active>
+              Tab 1
+            </Menu.Item>
+            <Menu.Item>
+              Tab 2
+            </Menu.Item>
+            <Menu.Item>
+              Tab 3
+            </Menu.Item>
+          </Menu>
+
+          <Header size="small">Secondary</Header>
+
+          <Menu secondary>
+            <Menu.Item active>
+              Tab 1
+            </Menu.Item>
+            <Menu.Item>
+              Tab 2
+            </Menu.Item>
+            <Menu.Item>
+              Tab 3
+            </Menu.Item>
+          </Menu>
+
+          <Header size="small">Secondary Pointing</Header>
+
+          <Menu secondary pointing>
+            <Menu.Item active>
+              Tab 1
+            </Menu.Item>
+            <Menu.Item>
+              Tab 2
+            </Menu.Item>
+            <Menu.Item>
+              Tab 3
+            </Menu.Item>
+          </Menu>
+        
+          <Header size="small">Primary Vertical</Header>
+
+          <Menu primary vertical>
+            <Menu.Item active>
+              Tab 1
+            </Menu.Item>
+            <Menu.Item>
+              Tab 2
+            </Menu.Item>
+            <Menu.Item>
+              Tab 3
+            </Menu.Item>
+          </Menu>
+
+          <Header size="small">Secondary Vertical</Header>
+
+          <Menu secondary vertical>
+            <Menu.Item active>
+              Tab 1
+            </Menu.Item>
+            <Menu.Item>
+              Tab 2
+            </Menu.Item>
+            <Menu.Item>
+              Tab 3
+            </Menu.Item>
+          </Menu>
+
+          <Header size="small">Secondary Vertical Pointing</Header>
+
+          <Menu secondary vertical pointing>
+            <Menu.Item active>
+              Tab 1
+            </Menu.Item>
+            <Menu.Item>
+              Tab 2
+            </Menu.Item>
+            <Menu.Item>
+              Tab 3
+            </Menu.Item>
+          </Menu>
+
+        </Segment>
+
+        <Segment>
+          <Header size="large">Divider</Header>
+          <Spacer size="small" />
+
+          <Header size="medium">Standard Divider</Header>
+
+          <Divider />
+          
+          <Header size="medium">Divider with Text</Header>
+
+          <Divider horizontal>OR</Divider>
+
+        </Segment>
+
+        <Segment>
+          <Header size="large">Breadcrumbs</Header>
+          <Spacer size="small" />
+
+          <Header size="medium">Standard Breadcrumbs</Header>
+
+          
+          <Breadcrumbs link="One" active="Two Active" />
+
+          <Breadcrumbs active="One Active" />
+        </Segment>
+
       </div>
     );
   }
