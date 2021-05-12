ac52284ad370ad1e2d22fc6431b51327cc15233e
Peter Cuhalev <peter@yukaii.com>
2021-05-12 12:57:49 +0200

    warning fix

--- services/web/src/layouts/Dashboard.js
+++ services/web/src/layouts/Dashboard.js
@@ -45,7 +45,7 @@ export default class DashboardLayout extends React.Component {
 
               <Layout style={{ flex:'0 0 auto', paddingTop:'0.5em', borderTop:'1px solid #ccc' }}>
               <Menu secondary vertical>
-                <Menu.Item as={NavLink} icon="terminal" to="/docs/getting-started">
+                <Menu.Item as={NavLink} to="/docs/getting-started">
                   <Icon name="terminal"/>Docs
                 </Menu.Item>
                 <Menu.Item as={NavLink} to="/settings">
