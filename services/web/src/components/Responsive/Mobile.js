48316744a3e8a7e13a284e37a3ca7290ebd252db
Andrew Plummer <plummer.andrew@gmail.com>
2021-05-22 19:04:04 -0400

    some more tweaks to docs layout

---
+++ services/web/src/components/Responsive/Mobile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './mobile.less';
+
+export default class Mobile extends React.Component {
+
+  render() {
+    return (
+      <div className="mobile">
+        {this.props.children}
+      </div>
+    );
+  }
+
+}
+
---
+++ services/web/src/components/Responsive/index.js
@@ -0,0 +1,3 @@
+export { default as Menu } from './Menu';
+export { default as Desktop } from './Desktop';
+export { default as Mobile } from './Mobile';
