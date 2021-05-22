31e8a771b0711f2c3b1332997d8078e47c797f13
Peter Cuhalev <peter@yukaii.com>
2021-05-22 10:15:37 +0200

    small fix to isolate menu style to only sidebar

--- services/web/src/layouts/Dashboard.js
+++ services/web/src/layouts/Dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withSession } from 'stores';
 import { NavLink } from 'react-router-dom';
-import { Menu, Icon, Container, Grid } from 'semantic';
+import { Menu, Icon, Container, Accordion } from 'semantic';
 import Footer from 'components/Footer';
 import { Layout } from 'components';
 import Protected from 'components/Protected';
@@ -9,11 +9,15 @@ import Sidebar from './Sidebar';
 
 import logo from 'assets/bedrock.svg';
 import favicon from 'assets/favicon.svg';
-import { SidebarPusher } from 'semantic-ui-react';
+import { AccordionPanel, SidebarPusher } from 'semantic-ui-react';
 
 @withSession
 export default class DashboardLayout extends React.Component {
+  
+
   render() {
+  
+
     return (
       <Sidebar>
         <Sidebar.Menu>
@@ -29,7 +33,26 @@ export default class DashboardLayout extends React.Component {
                 <Menu.Item as={NavLink} to="/shops">
                   <Icon name="store"/>Shops
                 </Menu.Item>
-                <Protected endpoint="events">
+
+                <Accordion as={Menu} secondary vertical fluid>
+                  <Accordion.Title
+                      // active={activeIndex === 0}
+                      index={0}
+                      // onClick={this.handleClick}
+                  >
+                    <Icon name="users" />Users
+                  </Accordion.Title>
+                  <Accordion.Content >
+                    <Menu.Item as={NavLink} to="/invites">
+                      <Icon name="envelope"/>Invites
+                    </Menu.Item>
+                    <Menu.Item as={NavLink} to="/docs">
+                      <Icon name="terminal"/>Docs
+                    </Menu.Item>
+                  </Accordion.Content>
+                </Accordion>
+
+                {/* <Protected endpoint="events">
                   <Menu.Item as={NavLink} to="/users">
                     <Icon name="users"/>Users
                   </Menu.Item>
@@ -38,7 +61,7 @@ export default class DashboardLayout extends React.Component {
                   <Menu.Item as={NavLink} to="/invites">
                     <Icon name="envelope"/>Invites
                   </Menu.Item>
-                </Protected>
+                </Protected> */}
               </Menu>
               <Layout style={{ flex:'0 0 auto', paddingTop:'15px', borderTop:'1px solid #ccc' }}>
                 <Menu secondary vertical fluid style={{ margin:'0', padding:'0 0 10px 25px' }}>
