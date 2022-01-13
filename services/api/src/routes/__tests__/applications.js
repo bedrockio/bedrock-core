fe1f393aa04e3d1a27cc6166fa12726d41f7f9a0
Kaare Larsen <me@kaareal.com>
2022-01-13 13:07:34 +0100

    more work

---
+++ services/api/src/routes/__tests__/applications.js
@@ -0,0 +1,97 @@
+const { setupDb, teardownDb, request, createUser, createAdminUser } = require('../../utils/testing');
+
+beforeAll(async () => {
+  await setupDb();
+});
+
+afterAll(async () => {
+  await teardownDb();
+});
+
+//XXX needs to be completed
+
+describe.skip('/1/applications', () => {
+  describe('POST /mine/search', () => {
+    it('should list audit logs', async () => {
+      const admin = await createAdminUser();
+      const response = await request('POST', '/1/audit-entries/search', {}, { user: admin });
+      expect(response.status).toBe(200);
+    });
+
+    it('should deny access to non-admins', async () => {
+      const user = await createUser({});
+      const response = await request('POST', '/1/audit-entries/search', {}, { user });
+      expect(response.status).toBe(401);
+    });
+  });
+
+  describe('POST /', () => {
+    it('should list audit logs', async () => {
+      const admin = await createAdminUser();
+      const response = await request('POST', '/1/audit-entries/search', {}, { user: admin });
+      expect(response.status).toBe(200);
+    });
+
+    it('should deny access to non-admins', async () => {
+      const user = await createUser({});
+      const response = await request('POST', '/1/audit-entries/search', {}, { user });
+      expect(response.status).toBe(401);
+    });
+  });
+
+  describe('POST /:application/logs/search', () => {
+    it('should list audit logs', async () => {
+      const admin = await createAdminUser();
+      const response = await request('POST', '/1/audit-entries/search', {}, { user: admin });
+      expect(response.status).toBe(200);
+    });
+
+    it('should deny access to non-admins', async () => {
+      const user = await createUser({});
+      const response = await request('POST', '/1/audit-entries/search', {}, { user });
+      expect(response.status).toBe(401);
+    });
+  });
+
+  describe('GET /:application', () => {
+    it('should list audit logs', async () => {
+      const admin = await createAdminUser();
+      const response = await request('POST', '/1/audit-entries/search', {}, { user: admin });
+      expect(response.status).toBe(200);
+    });
+
+    it('should deny access to non-admins', async () => {
+      const user = await createUser({});
+      const response = await request('POST', '/1/audit-entries/search', {}, { user });
+      expect(response.status).toBe(401);
+    });
+  });
+
+  describe('PATCH /:application', () => {
+    it('should list audit logs', async () => {
+      const admin = await createAdminUser();
+      const response = await request('POST', '/1/audit-entries/search', {}, { user: admin });
+      expect(response.status).toBe(200);
+    });
+
+    it('should deny access to non-admins', async () => {
+      const user = await createUser({});
+      const response = await request('POST', '/1/audit-entries/search', {}, { user });
+      expect(response.status).toBe(401);
+    });
+  });
+
+  describe('DELETE /:application', () => {
+    it('should list audit logs', async () => {
+      const admin = await createAdminUser();
+      const response = await request('POST', '/1/audit-entries/search', {}, { user: admin });
+      expect(response.status).toBe(200);
+    });
+
+    it('should deny access to non-admins', async () => {
+      const user = await createUser({});
+      const response = await request('POST', '/1/audit-entries/search', {}, { user });
+      expect(response.status).toBe(401);
+    });
+  });
+});
