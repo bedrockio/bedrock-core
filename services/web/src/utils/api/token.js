2b0fd8db59b38816e9f804285f319336347ffe20
Kaare Larsen <me@kaareal.com>
2022-01-13 11:46:06 +0100

    snake case

--- services/web/src/utils/api/token.js
+++ services/web/src/utils/api/token.js
@@ -1,7 +1,7 @@
 import { APP_NAME } from 'utils/env';
-import { camelCase } from 'lodash';
+import { snakeCase } from 'lodash';
 
-const KEY = `${camelCase(APP_NAME)}JWT`;
+const KEY = `${snakeCase(APP_NAME)}_jwt`;
 
 export function hasToken() {
   return !!getToken();
