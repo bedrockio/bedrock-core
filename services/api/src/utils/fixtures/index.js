473e14c1583417b6c1fdfa1230f504c50945b988
Kaare <kaareal@users.noreply.github.com>
2022-04-17 13:29:43 +0200

    use get instead, for warnings

--- services/api/src/utils/fixtures/index.js
+++ services/api/src/utils/fixtures/index.js
@@ -13,7 +13,9 @@ const { exportFixtures } = require('./export');
 const { pluralCamel, pluralKebab } = require('./utils');
 const models = require('./models');
 
-const { ADMIN_EMAIL, API_URL } = config.getAll();
+const ADMIN_EMAIL = config.get("ADMIN_EMAIL");
+const API_URL = config.get("API_URL");
+
 const { BASE_DIR, CUSTOM_TRANSFORMS, MODEL_TRANSFORMS, ADMIN_FIXTURE_ID } = require('./const');
 
 // Loads fixtures once if not loaded and returns true/false.
