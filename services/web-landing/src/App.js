5d50ae7671644fef139ce24b15f41e1465eb9f78
Kaare Larsen <me@kaareal.com>
2022-06-03 12:00:37 +0200

    web-landing

---
+++ services/web-landing/src/App.js
@@ -0,0 +1,45 @@
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>That feels like an existential question, don't you think?</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
