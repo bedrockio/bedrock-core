0666b9d2a107299244aaa699deccbff91e5eb25b
Kaare <kaareal@users.noreply.github.com>
2022-08-03 11:55:35 +0200

    adding isDeveloper (#209)
    * adding isDeveloper
    
    * adding docs

--- services/web/src/modals/EditUser.js
+++ services/web/src/modals/EditUser.js
@@ -112,6 +112,13 @@ export default class EditUser extends React.Component {
                 value={user.roles || []}
                 onChange={(e, { value }) => this.setField('roles', value)}
               />
+              <Form.Checkbox
+                checked={user.isDeveloper || false}
+                label="Is Developer"
+                onChange={(e, { checked }) =>
+                  this.setField('isDeveloper', checked)
+                }
+              />
             </Form>
           </AutoFocus>
         </Modal.Content>
