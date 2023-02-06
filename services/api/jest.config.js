8f4ec7ccc7d6a5b93672c28231503e538fe4e769
Andrew Plummer <plummer.andrew@gmail.com>
2023-02-06 10:47:20 +0900

    bumped node

--- services/api/jest.config.js
+++ services/api/jest.config.js
@@ -3,6 +3,7 @@ process.env.LOG_LEVEL = 'warn';
 module.exports = {
   testTimeout: 60000,
   preset: '@shelf/jest-mongodb',
+  // Only run on changed files without extra arguments.
   watchPlugins: ['./src/utils/testing/ChangedFilesPlugin'],
   // https://github.com/shelfio/jest-mongodb#6-jest-watch-mode-gotcha
   watchPathIgnorePatterns: ['globalConfig'],
