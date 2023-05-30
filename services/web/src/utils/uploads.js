c956f22df0eb19a7a85b1199a8b76c8be9226eeb
Kaare Larsen <me@kaareal.com>
2023-05-30 12:18:14 +0200

    improved check

--- services/web/src/utils/uploads.js
+++ services/web/src/utils/uploads.js
@@ -1,6 +1,6 @@
 import { API_URL } from 'utils/env';
 
 export function urlForUpload(upload) {
-  const url = `/1/uploads/${upload.id || upload}/raw`;
+  const url = `/1/uploads/${upload?.id || upload}/raw`;
   return new URL(url, API_URL).toString();
 }
