bbe63951b38498e77aa522dfbf9047fd0ad9d0e9
Andrew Plummer <plummer.andrew@gmail.com>
2023-06-03 23:05:50 +0900

    fix for serialize middleware

--- services/api/src/utils/middleware/serialize.js
+++ services/api/src/utils/middleware/serialize.js
@@ -2,7 +2,11 @@ const { serializeObject } = require('../serialize');
 
 async function serialize(ctx, next) {
   await next();
-  ctx.body = serializeObject(ctx.body, ctx);
+  // Setting ctx.body always results in a 204 so only
+  // proceed with serialize if it has been set.
+  if (ctx.body) {
+    ctx.body = serializeObject(ctx.body, ctx);
+  }
 }
 
 module.exports = serialize;
