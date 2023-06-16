d1c6371d9ce51aa309ab55ee6404f351fa8fdf7b
Andrew Plummer <plummer.andrew@gmail.com>
2023-06-16 20:54:15 +0900

    fixed web tests

--- services/web/jest.config.js
+++ services/web/jest.config.js
@@ -1,6 +1,7 @@
 process.env.ENV_NAME = 'test';
 
 module.exports = {
+  testEnvironment: 'jsdom',
   moduleDirectories: ['node_modules', './src/', './serve'],
   testPathIgnorePatterns: ['node_modules', '\\w+.ignore.js'],
   moduleNameMapper: {
