22732a14e1e29611f3b4a51ccf7af0e8c9eef14f
Kaare Larsen <me@kaareal.com>
2025-02-09 11:35:32 +0100

    work in progress

--- services/web/postcss.config.js
+++ services/web/postcss.config.js
@@ -1,15 +1,14 @@
 module.exports = {
   plugins: {
-    autoprefixer: {},
-    cssnano: {
-      preset: [
-        'default',
-        {
-          discardComments: {
-            removeAll: true,
-          },
-        },
-      ],
+    'postcss-preset-mantine': {},
+    'postcss-simple-vars': {
+      variables: {
+        'mantine-breakpoint-xs': '36em',
+        'mantine-breakpoint-sm': '48em',
+        'mantine-breakpoint-md': '62em',
+        'mantine-breakpoint-lg': '75em',
+        'mantine-breakpoint-xl': '88em',
+      },
     },
   },
 };
