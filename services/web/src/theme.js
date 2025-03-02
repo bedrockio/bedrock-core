90b13297bca509894dc5e8cae18c3afaa7606b8a
Kaare Larsen <me@kaareal.com>
2025-03-02 11:18:06 +0100

    theme

--- services/web/src/theme.js
+++ services/web/src/theme.js
@@ -1,5 +1,19 @@
 import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
-  primaryColor: 'green',
+  primaryColor: 'primary',
+  colors: {
+    primary: [
+      '#f3fbea',
+      '#e7f2db',
+      '#cfe3b7',
+      '#b4d491',
+      '#9ec670',
+      '#90be5b',
+      '#88ba4f',
+      '#74a340',
+      '#669136',
+      '#567e29',
+    ],
+  },
 });
