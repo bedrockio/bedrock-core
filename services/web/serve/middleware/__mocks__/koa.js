11d3c9d13991cd069e1fe89af47fc16c96151f6c
Kaare Larsen <me@kaareal.com>
2025-03-05 21:41:28 +0100

    more work

---
+++ services/web/serve/middleware/__mocks__/koa.js
@@ -0,0 +1,34 @@
+const Koa = require('koa');
+const httpMocks = require('node-mocks-http');
+
+const app = new Koa();
+
+class FakeCookies {
+  constructor(data = {}) {
+    this.data = data;
+  }
+
+  get(key) {
+    return this.data[key];
+  }
+
+  set(key, val) {
+    this.data[key] = val;
+  }
+}
+
+export function createFakeKoaContext(mockReq = {}, mockRes = {}) {
+  // Koa checks this so assign as mock value
+  Object.assign(mockReq, { socket: {} });
+
+  const ctx = app.createContext(
+    httpMocks.createRequest(mockReq),
+    httpMocks.createResponse(mockRes),
+  );
+  console.log('123123', ctx);
+
+  // Something internal to the http module breaks if we don't set this.
+  ctx.cookies = new FakeCookies(mockReq.cookies);
+
+  return ctx;
+}
