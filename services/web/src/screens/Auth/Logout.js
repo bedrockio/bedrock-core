1bb0b4506098bbe8b00b5e71f6034333be251cab
Kaare Larsen <me@kaareal.com>
2025-05-07 14:00:59 +0200

    fixing

--- services/web/src/screens/Auth/Logout.js
+++ services/web/src/screens/Auth/Logout.js
@@ -5,11 +5,7 @@ export default function Logout() {
   const session = useSession();
 
   useEffect(() => {
-    const logoutUser = async () => {
-      await session.logout();
-    };
-
-    logoutUser();
+    session.logout();
   }, [session]);
 
   return <div />;
