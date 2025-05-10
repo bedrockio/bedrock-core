9b1efb256115144b6a1d59351757034dd92088d1
Kaare <kaareal@users.noreply.github.com>
2025-05-10 15:53:51 +0200

    mongo:8 (#262)

--- services/api/src/utils/database.js
+++ services/api/src/utils/database.js
@@ -4,23 +4,7 @@ const logger = require('@bedrockio/logger');
 
 mongoose.Promise = Promise;
 
-// https://mongoosejs.com/docs/migrating_to_6.html#no-more-deprecation-warning-options
-// No More Deprecation Warning Options
-// useNewUrlParser, useUnifiedTopology, useFindAndModify, and useCreateIndex are no longer supported options. Mongoose 6 always behaves as if useNewUrlParser, useUnifiedTopology, and useCreateIndex are true, and useFindAndModify is false. Please remove these options from your code.
-
-const flags = {
-  // // The underlying MongoDB driver has deprecated their current connection string parser.
-  // useNewUrlParser: true,
-  // // Make Mongoose's default index build use createIndex() instead of ensureIndex()
-  // // to avoid deprecation warnings from the MongoDB driver
-  // useCreateIndex: true,
-  // // To opt in to using the MongoDB driver's new connection management engine.
-  // // https://mongoosejs.com/docs/deprecations.html#useunifiedtopology
-  // useUnifiedTopology: true,
-  // // Set to false to make findOneAndUpdate() and findOneAndRemove()
-  // // use native findOneAndUpdate() rather than findAndModify()
-  // useFindAndModify: false,
-};
+const flags = {};
 exports.flags = flags;
 
 exports.initialize = async function initialize() {
